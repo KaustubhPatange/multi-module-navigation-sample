ca53e5bfc89a953ef098620d441fad9423877b34
Kaustubh Patange <developerkp16@gmail.com>
2021-10-29 11:13:49 +0530

    local commit

---
+++ app/src/main/java/com/kpstv/navigator_multimodule_test/FirstFragment.kt
@@ -0,0 +1,19 @@
+package com.kpstv.navigator_multimodule_test
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kpstv.navigator_multimodule_test.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+  }
+}
